c5550be849d8e8f1617b4ad9b4bdb69d7e464b14
HYUN JUN YOOK <yhj03069@gmail.com>
2023-02-07 09:59:31 +0900

    Initial Commit

---
+++ 2023circleTracking2/_023circleTracking2App.swift
@@ -0,0 +1,17 @@
+//
+//  _023circleTracking2App.swift
+//  2023circleTracking2
+//
+//  Created by cnu on 2023/02/07.
+//
+
+import SwiftUI
+
+@main
+struct _023circleTracking2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
